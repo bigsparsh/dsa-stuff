31adbf5ab0b3baffee9705971099fa8f8148bfc3
Sparsh Singh <sparshsingh7586@gmail.com>
2024-11-07 08:12:32 +0530

    Edited some program

--- linked_list/reverse-linked.c
+++ linked_list/reverse-linked.c
@@ -7,12 +7,15 @@ typedef struct node {
 } NODE;
 
 NODE* insert (NODE *);
+NODE* altNodes (NODE *);
 NODE* reverse (NODE *, NODE *);
 void display (NODE *);
+int checkPallindrome (NODE *, NODE **);
+NODE *taill = NULL;
 
 int main () {
   NODE *head = NULL, *tail = NULL, *temp = NULL;
-  int choice;
+  int choice, isPallindrome;
   
   do {
     printf("\n===[REVERSING LINKED LIST]===\n 1. Insert\n 2. Display\n 3. Reverse\n 4. Exit\nEnter choice (1-4): ");
@@ -31,12 +34,19 @@ int main () {
         tail = temp;
         break;
       case 4:
+        NODE *taill = head;
+        if (checkPallindrome(head, &taill) == 1)
+            printf("\nLinked List is a Pallindrome\n");
+        else
+            printf("\nLinked List is not a Pallindrome\n");
         break;
+      case 5:
+        head = altNodes(head);
       default:
         printf("\nInvalid Choice\n");
         break;
     }
-  } while (choice != 4);
+  } while (choice != 7);
 
 
 }
@@ -52,15 +62,45 @@ NODE* insert (NODE *tail) {
   return tail;
 }
 
-NODE* reverse (NODE* head, NODE *tail) {
-  NODE *h = head;
-  while (h != tail) {
-    head = head->next;
-    h->next = tail->next;
-    tail->next = h;
-    h = head;
+// NODE* reverse (NODE* head, NODE *tail) {
+//   NODE *h = head;
+//   while (h != tail) {
+//     head = head->next;
+//     h->next = tail->next;
+//     tail->next = h;
+//     h = head;
+//   }
+//   return head;
+// }
+
+NODE* reverse (NODE *head, NODE *tail) {
+  NODE *h = head, *c = head;
+  while (c->next != NULL) {
+    c = c->next;
+    h->next = c->next;
+    c->next = head;
+    head = c;
+    c = h;
   }
   return head;
+}
+
+NODE *altNodes (NODE *head) {
+    NODE *temp = head;
+    while (temp != NULL && temp->next != NULL) {
+        temp->next = temp->next->next;
+        temp = temp->next;
+    }
+    return head;
+}
+
+int checkPallindrome(NODE* head, NODE **tail) {
+    if (head == NULL) return 1;
+    int isPal = checkPallindrome(head->next, tail);
+    if (isPal == -1) return -1;
+    int isDataEqual = ((*tail)->data == head->data ? 1 : -1);
+    *tail = (*tail)->next;
+    return isDataEqual;
 }
 
 
