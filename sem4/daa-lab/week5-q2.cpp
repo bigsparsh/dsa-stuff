994d67791c91e24f01b865673bdaff1b78fef34f
bigsparsh <sparshsingh7586@gmail.com>
2025-03-13 15:43:44 +0530

    Changed a DAA program

--- sem4/daa-lab/week5-q2.cpp
+++ sem4/daa-lab/week5-q2.cpp
@@ -11,40 +11,77 @@
 // The output will have T number of lines.
 // For each test case, output will be the elements arr[i] and arr[j] such that
 // arr[i]+arr[j] = key if exist otherwise print 'No Such Elements Exist”.
+// use heap sort
 
-#include <algorithm>
 #include <iostream>
 using namespace std;
 
-void findSum(int arr[], int n, int key) {
-  sort(arr, arr + n);
-  int i = 0, j = n - 1;
-  while (i < j) {
-    if (arr[i] + arr[j] == key) {
-      cout << arr[i] << " " << arr[j] << endl;
+void heapify(int arr[], int n, int i) {
+  int largest = i;
+  int l = 2 * i + 1;
+  int r = 2 * i + 2;
+
+  if (l < n && arr[l] > arr[largest])
+    largest = l;
+
+  if (r < n && arr[r] > arr[largest])
+    largest = r;
+
+  if (largest != i) {
+    swap(arr[i], arr[largest]);
+    heapify(arr, n, largest);
+  }
+}
+
+void heapSort(int arr[], int n) {
+  for (int i = n / 2 - 1; i >= 0; i--) {
+    heapify(arr, n, i);
+  }
+
+  for (int i = n - 1; i > 0; i--) {
+    swap(arr[0], arr[i]);
+    heapify(arr, i, 0);
+  }
+}
+
+void findPair(int arr[], int n, int key) {
+  heapSort(arr, n);
+
+  int low = 0;
+  int high = n - 1;
+
+  while (low < high) {
+    if (arr[low] + arr[high] == key) {
+      cout << arr[low] << " " << arr[high] << endl;
       return;
-    } else if (arr[i] + arr[j] < key) {
-      i++;
+    } else if (arr[low] + arr[high] < key) {
+      low++;
     } else {
-      j--;
+      high--;
     }
   }
+
   cout << "No Such Elements Exist" << endl;
 }
 
 int main() {
   int t;
   cin >> t;
+
   while (t--) {
     int n;
     cin >> n;
+
     int arr[n];
     for (int i = 0; i < n; i++) {
       cin >> arr[i];
     }
+
     int key;
     cin >> key;
-    findSum(arr, n, key);
+
+    findPair(arr, n, key);
   }
+
   return 0;
 }
